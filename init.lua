776b34536a2f16396e7d3ebc0014df1a4f6e2db6
ktkim1 <ktkim1@humax-networks.com>
2025-01-08 10:50:42 +0900

    tag tag c tag
    Change-Id: I66c5cf4a3d896cdaf6873f4f535e3d835b8ec853

--- init.lua
+++ init.lua
@@ -98,6 +98,9 @@ vim.g.have_nerd_font = false
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- yeah ctags bro
+vim.opt.tags = '/.tags;/'
+
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.

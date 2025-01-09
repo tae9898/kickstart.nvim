e9d0194e5011c2682dea8f39a56a3c3722929a0e
ktkim1 <ktkim1@humax-networks.com>
2025-01-09 13:15:19 +0900

    Enable to yank path by neo tree
    Change-Id: If92967a40cc44cde15d712f8218d2c7979d036b5

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -18,6 +18,21 @@ return {
       window = {
         mappings = {
           ['\\'] = 'close_window',
+          ['y'] = function(state)
+            -- 선택된 노드의 경로를 가져옴
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            -- 시스템 클립보드에 복사
+            vim.fn.setreg('+', filepath)
+            vim.notify('Copied: ' .. filepath)
+          end,
+          -- 상대 경로용 매핑을 추가하고 싶다면
+          ['yr'] = function(state)
+            local node = state.tree:get_node()
+            local filepath = vim.fn.fnamemodify(node:get_id(), ':.')
+            vim.fn.setreg('+', filepath)
+            vim.notify('Copied relative: ' .. filepath)
+          end,
         },
       },
     },

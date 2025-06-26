7b37abb7b0acc17d663beb4b595a2be586d6696f
ktkim1 <ktkim1@humax-networks.com>
2025-06-26 10:56:07 +0900

    Delete commit : Configure delete commands to not affect clipboard
    Change-Id: I25d43924c3ee4aa58f5f8cea17ad1612d902bdbc

--- init.lua
+++ init.lua
@@ -202,11 +202,6 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 --  See `:help lua-guide-autocommands`
 -- i don't wanna save at clipboard when i use 'x'
 vim.keymap.set('n', 'x', '"_x', { noremap = true })
--- i don't wanna save at clipboard when i use 'd'
-vim.keymap.set('n', 'd', '"_d')
-vim.keymap.set('v', 'd', '"_d')
-vim.keymap.set('n', 'D', '"_D')
-vim.keymap.set('n', 'dd', '"_dd')
 
 -- and i don't wanna \n when "yy"
 vim.keymap.set('n', 'yy', function()

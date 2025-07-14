38a32294a156018d6e377d96bd068ae6cf9b360d
ktkim1 <ktkim1@humax-networks.com>
2025-07-14 10:33:48 +0900

    Add neovim chat gpt pamphlet
    Change-Id: If6d1b13d2b2f7fa25a14c6a91d9d9d80afec133d

--- init.lua
+++ init.lua
@@ -228,7 +228,26 @@ vim.keymap.set('n', '<leader>gd', ':ChatGPTRun docstring<CR>', { noremap = true,
 vim.keymap.set('v', '<leader>gd', ':ChatGPTRun docstring<CR>', { noremap = true, silent = true, desc = 'Docstring' })
 vim.keymap.set('n', '<leader>gx', ':ChatGPTRun explain_code<CR>', { noremap = true, silent = true, desc = 'Explain code' })
 vim.keymap.set('v', '<leader>gx', ':ChatGPTRun explain_code<CR>', { noremap = true, silent = true, desc = 'Explain code' })
+--[[
 
+    <C-Enter> [Both] to submit.
+    <C-y> [Both] to copy/yank last answer.
+    <C-o> [Both] Toggle settings window.
+    <C-h> [Both] Toggle help window.
+    <Tab> [Both] Cycle over windows.
+    <C-f> [Chat] Cycle over modes (center, stick to right).
+    <C-c> [Both] to close chat window.
+    <C-p> [Chat] Toggle sessions list.
+    <C-u> [Chat] scroll up chat window.
+    <C-d> [Chat] scroll down chat window.
+    <C-k> [Chat] to copy/yank code from last answer.
+    <C-n> [Chat] Start new session.
+    <C-r> [Chat] draft message (create message without submitting it to server)
+    <C-r> [Chat] switch role (switch between user and assistant role to define a workflow)
+    <C-s> [Both] Toggle system message window.
+    <C-i> [Edit Window] use response as input.
+    <C-d> [Edit Window] view the diff between left and right panes and use diff-mode commands
+]]
 --Change the world keymap
 vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
     desc = "Toggle Spectre"

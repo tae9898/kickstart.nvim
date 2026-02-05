b5d10e5798f282e7f41637d03649cf4917d8dc27
ktkim1 <ktkim1@humax-networks.com>
2026-02-05 10:40:38 +0900

    Add : Prime's ai tool 99
    Change-Id: I55f2e4919f0a361a6bb45c0a61fdb14372df0ea1

--- init.lua
+++ init.lua
@@ -346,6 +346,110 @@ require('lazy').setup({
   --    dependencies = { "nvim-lua/plenary.nvim" }
   --   }
   -- add some git plugin
+
+  -- prime's ai tool 
+  -- need nvim-cmp also 
+
+{
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      -- 필요한 경우 추가 소스들
+    },
+    config = function()
+      -- 기본적인 cmp 설정이 필요할 수 있습니다.
+    end
+  },
+	{
+		"ThePrimeagen/99",
+		config = function()
+			local _99 = require("99")
+
+            -- For logging that is to a file if you wish to trace through requests
+            -- for reporting bugs, i would not rely on this, but instead the provided
+            -- logging mechanisms within 99.  This is for more debugging purposes
+            local cwd = vim.uv.cwd()
+            local basename = vim.fs.basename(cwd)
+			_99.setup({
+				logger = {
+					level = _99.DEBUG,
+					path = "/tmp/" .. basename .. ".99.debug",
+					print_on_error = true,
+				},
+
+                --- A new feature that is centered around tags
+                completion = {
+                    --- Defaults to .cursor/rules
+                    -- I am going to disable these until i understand the
+                    -- problem better.  Inside of cursor rules there is also
+                    -- application rules, which means i need to apply these
+                    -- differently
+                    -- cursor_rules = "<custom path to cursor rules>"
+
+                    --- A list of folders where you have your own SKILL.md
+                    --- Expected format:
+                    --- /path/to/dir/<skill_name>/SKILL.md
+                    ---
+                    --- Example:
+                    --- Input Path:
+                    --- "scratch/custom_rules/"
+                    ---
+                    --- Output Rules:
+                    --- {path = "scratch/custom_rules/vim/SKILL.md", name = "vim"},
+                    --- ... the other rules in that dir ...
+                    ---
+                    custom_rules = {
+                      "scratch/custom_rules/",
+                    },
+
+                    --- What autocomplete do you use.  We currently only
+                    --- support cmp right now
+                    source = "cmp",
+                },
+
+                --- WARNING: if you change cwd then this is likely broken
+                --- ill likely fix this in a later change
+                ---
+                --- md_files is a list of files to look for and auto add based on the location
+                --- of the originating request.  That means if you are at /foo/bar/baz.lua
+                --- the system will automagically look for:
+                --- /foo/bar/AGENT.md
+                --- /foo/AGENT.md
+                --- assuming that /foo is project root (based on cwd)
+				md_files = {
+					"AGENT.md",
+				},
+			})
+
+            -- Create your own short cuts for the different types of actions
+			vim.keymap.set("n", "<leader>9f", function()
+				_99.fill_in_function()
+			end)
+            -- take extra note that i have visual selection only in v mode
+            -- technically whatever your last visual selection is, will be used
+            -- so i have this set to visual mode so i dont screw up and use an
+            -- old visual selection
+            --
+            -- likely ill add a mode check and assert on required visual mode
+            -- so just prepare for it now
+			vim.keymap.set("v", "<leader>9v", function()
+				_99.visual()
+			end)
+
+            --- if you have a request you dont want to make any changes, just cancel it
+			vim.keymap.set("v", "<leader>9s", function()
+				_99.stop_all_requests()
+			end)
+
+            --- Example: Using rules + actions for custom behaviors
+            --- Create a rule file like ~/.rules/debug.md that defines custom behavior.
+            --- For instance, a "debug" rule could automatically add printf statements
+            --- throughout a function to help debug its execution flow.
+			vim.keymap.set("n", "<leader>9fd", function()
+				_99.fill_in_function()
+			end)
+		end,
+	},
   {
     'tpope/vim-fugitive',
   },
@@ -798,19 +902,24 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
 clangd = {
-    -- 1. 로그에 AST 에러가 너무 많으므로 백그라운드 인덱싱 옵션도 추가하면 좋습니다.
-    cmd = {
-        "clangd",
-        "--background-index",
-        "--clang-tidy",
-        "--header-insertion=iwyu",
-        "--completion-style=detailed",
-        "--function-arg-placeholders",
-        "--fallback-style=llvm",
-        -- 👇 [중요] 여기에 님 툴체인 경로를 와일드카드로 적어주세요
-        "--query-driver=/usr/bin/*gcc*,/opt/toolchains/**/*gcc*,/home/ktkim/**/*gcc*" 
-    },
+--    -- 1. 로그에 AST 에러가 너무 많으므로 백그라운드 인덱싱 옵션도 추가하면 좋습니다.
+--    cmd = {
+--        "clangd",
+--        "--background-index",
+--        "--clang-tidy",
+--        "--header-insertion=iwyu",
+--        "--completion-style=detailed",
+--        "--function-arg-placeholders",
+--        "--fallback-style=llvm",
+--        -- 👇 [중요] 여기에 님 툴체인 경로를 와일드카드로 적어주세요
+--        "--query-driver=/usr/bin/*gcc*,/opt/toolchains/**/*gcc*,/home/ktkim/**/*gcc*"
+--    },
+--    capabilities = {
+--      offsetEncoding = { "utf-16" },
+--    },
 },
+
+
         -- gopls = {},
         pyright = {},
         -- rust_analyzer = {},
@@ -873,7 +982,11 @@ clangd = {
       }
     end,
   },
-
+{
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {},
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, 
+},
   { -- Autoformat
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
